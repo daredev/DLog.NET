9ce39a366544182f1e08838840a88bb5d7ae939b
daredev <piczok@gmail.com>
2015-02-26 16:05:47 +0100

    Baseline added

---
+++ DLog.NET/Models/LogMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DLog.NET.Models
+{
+    public class LogMessage
+    {
+        private readonly DateTime _timestamp;
+        private readonly string _message;
+
+        public DateTime TimeStamp { get { return _timestamp; } }
+        public string Message { get { return _message; } }
+
+        public LogMessage(string message)
+            : this()
+        {
+            _message = message;
+        }
+
+        private LogMessage()
+        {
+            _timestamp = DateTime.Now;
+            _message = "";
+        }
+
+        public string GetFormatted()
+        {
+            return string.Format("[{0:d/M/yyyy HH:mm:ss}]: {1}", TimeStamp, Message);
+        }
+    }
+}
